a5cb747c7f526994498962c0d73560cbc5bc70aa
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2022-02-22 19:50:00 -0800

    finished earthquakes table

---
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -0,0 +1,20 @@
+const earthquakesFixtures = {
+    twoEarthquakes: [
+       {
+         "id": "abcd1234abcd1234abcd1234",
+         "title": "M 2.2 - 10km ESE of Ojai, CA",
+         "mag": 2.16,
+         "place": "10km ESE of Ojai, CA",
+         "time": 1644571919000
+       },
+       {
+        "id": "abcd5678abcd5678abcd5678",
+        "title": "V 2.2 - 11km ESE of LA, CA",
+        "mag": 4.2,
+        "place": "11km ESE of LA, CA",
+        "time": 1644571919023
+      }
+     ]
+   }
+   
+   export {earthquakesFixtures }; 
